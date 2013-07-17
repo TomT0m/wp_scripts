3b61441cd1364039bb32e1d06031310c46bbabd1
Thomas Douillard <thomas.douillard@gmail.com>
2013-07-17 10:52:56 +0200

    serie management

--- pywikipedia/wikidata_label_serie_season.py
+++ pywikipedia/wikidata_label_serie_season.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python
+#encoding: utf-8
 """ 
 Serie formatting in wikidata
 """
@@ -7,7 +8,24 @@ Serie formatting in wikidata
 import pywikibot
 # create a site object, here for en-wiki
 import sys
+import logging
 
+def set_lang_label(page, serie_name, season_num, lang, descr):
+	""" per language labelling """
+	datas = page.get()
+	if ('label' not in datas or 
+		not datas['label'][lang] or 
+		datas['label'][lang] == serie_name):
+			page.setitem(summary=u"serie season label disambiguation",
+				items={'type': u'item', 'label': 'en', 'value': '{} season {}'.format(serie_name, season_num) })
+	else:
+		logging.info("doing nothing")
+		print("doing nothing")
+
+def set_season_labels(page, serie_name, season_num):
+	""" setting labels """
+	set_lang_label(page, serie_name, season_num, 'en', '{} season {}'.format(serie_name, season_num))
+	set_lang_label(page, serie_name, season_num, 'fr', '{} saison {}'.format(serie_name, season_num))
 
 
 def main(serie_name, site_name = 'en' ):
@@ -33,18 +51,23 @@ def main(serie_name, site_name = 'en' ):
 		page = pywikibot.Page(site, title)
 		print(title)
 		page = pywikibot.Page(site, title)
-		print(page.get())
-		print("plop")
 		if page.exists():
 			datapage = pywikibot.DataPage(page)
 			if datapage.exists():
-				items[current] = datapage.get()
-				current += 1
+				datapage.get()
+				items[current] = datapage
 			else:
 				raise Exception()
+			
 		else:
 			has_previous = False
+
+		current += 1
+
 	print("Number of seasons : {}".format(current))
+
+	for i in range(1, len(items)):
+		set_season_labels(items[i], serie_name, i)
 
 main(sys.argv[1])
 
